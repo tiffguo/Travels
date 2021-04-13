89f48bec0acf43ba630efe20919b44a8a860910e
Tiffany Guo <tiffguo.@umich.edu>
2021-04-13 16:11:06 -0400

    newer changes

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function toggle () {  
-    console.log("In the toggle function")
-    // Declare variable menu
-    let menu = document.getElementById("the-menu");
-    console.log(menu.style.display)
-    // toggle code
-   if (menu.style.display === "block") {
-      menu.style.display = "none";
-    }
-    else{
-      menu.style.display = "block"; 
-    }
-  } 
+// function toggle () {  
+//     console.log("In the toggle function")
+//     // Declare variable menu
+//     let menu = document.getElementById("the-menu");
+//     console.log(menu.style.display)
+//     // toggle code
+//    if (menu.style.display === "block") {
+//       menu.style.display = "none";
+//     }
+//     else{
+//       menu.style.display = "block"; 
+//     }
+//   } 
 
 
 
